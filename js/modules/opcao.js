7451bfd899b2c36fb5d88fef25f5c08e4b916eb6
ralphdosreis <ralphdosreistorres@gmail.com>
2024-07-26 15:32:24 -0300

    interacao opcao-matricula criada

---
+++ js/modules/opcao.js
@@ -0,0 +1,21 @@
+export default function initActiveOption() {
+  const opcoes = document.querySelectorAll('.opcao li');
+  const escolha = document.querySelectorAll('.container-aula-matricula');
+
+  escolha[0].classList.add('ativo');
+
+  if (opcoes && escolha) {
+    function activeEscolha(index) {
+      escolha.forEach((escolhido) => {
+        escolhido.classList.remove('ativo');
+      });
+      escolha[index].classList.add('ativo');
+    }
+
+    opcoes.forEach((opcao, index) => {
+      opcao.addEventListener('click', () => {
+        activeEscolha(index);
+      });
+    });
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 import initScrollSuave from './modules/scroll-suave.js';
+import initActiveOption from './modules/opcao.js';
+
 initScrollSuave();
+initActiveOption();
