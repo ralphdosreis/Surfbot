a74d30e21ca9c39df5932a1e8e8b6ff042be2d10
ralphdosreis <ralphdosreistorres@gmail.com>
2024-07-30 15:51:27 -0300

    first-commit lógica

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initScrollSuave from './modules/scroll-suave.js';
 import initActiveOption from './modules/opcao.js';
+import initAnimaValor from './modules/valor-debitado.js';
 
 initScrollSuave();
 initActiveOption();
+initAnimaValor();
