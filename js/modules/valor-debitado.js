2bba4e0a6e4f0fb45ffbd203d3fed69f7c53ef66
ralphdosreis <ralphdosreistorres@gmail.com>
2024-08-06 17:44:28 -0300

    erro webpack

--- js/modules/valor-debitado.js
+++ js/modules/valor-debitado.js
@@ -1,9 +1,9 @@
 export default function initAnimaValor() {}
 
-const valorOpcao = document.querySelectorAll('.aula-valor');
-const valorDebitado = document.querySelector('.aviso-debito');
+// const valorOpcao = document.querySelectorAll('.aula-valor');
+// const valorDebitado = document.querySelector('.aviso-debito');
 
-valorOpcao.forEach((valor) => {
-  const limpaPreco = valor.innerText.replace(' / Aula', '');
-  valorDebitado.innerText = `Seu cartão será debitado em ${limpaPreco}`;
-});
+// valorOpcao.forEach((valor) => {
+//   const limpaPreco = valor.innerText.replace(' / Aula', '');
+//   valorDebitado.innerText = `Seu cartão será debitado em ${limpaPreco}`;
+// });
